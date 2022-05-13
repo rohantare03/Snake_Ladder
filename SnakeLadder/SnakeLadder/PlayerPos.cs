6133c7f86773c5437618c318ea1375bf29b9148a
rohantare03 <rohatare123@gmail.com>
2022-05-13 16:47:41 +0530

    [Rohan] Add : Starting position of player

---
+++ SnakeLadder/SnakeLadder/PlayerPos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class PlayerPos
+    {
+        public void PlayerPosition()
+        {
+            int Playerposition = 0;
+            Console.WriteLine("Starting position of player is " + Playerposition);
+
+        }
+    }
+}
---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a particular number : ");
+            Console.WriteLine("1 for Player position.");
+
+            int Problem = Convert.ToInt32(Console.ReadLine());
+            switch (Problem)
+            {
+                case 1:
+                    PlayerPos playerPosition = new PlayerPos();
+                    playerPosition.PlayerPosition();
+                    break;
+            }
+        }
+    }
+}
